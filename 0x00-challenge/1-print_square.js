fe75cf1b0db850f2b4da4f29d623ec7f4a25b926
alzheimeer <fogniebla@hotmail.com>
2019-12-18 21:30:16 +0000

    feature task 1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
@@ -5,7 +7,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 16)
+size = parseInt(process.argv[2])
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
